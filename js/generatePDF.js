f7db89f4313c5e41b0edc77a253b7705034aa2a0
doweknowenough <148193641+doweknowenough@users.noreply.github.com>
2026-07-28 16:23:30 +0530

    Update generatePDF.js

--- js/generatePDF.js
+++ js/generatePDF.js
@@ -56,7 +56,8 @@ function generateQCABPDF(questions) {
 
             // Footer
             const footerText = `XXXX-${q.question_id}`;
-            const footerText2 = `XXXX-${page++}`;
+            const footerText2 = `XXXX-${page}`;
+            page++;
             doc.setFontSize(8);
             doc.text(footerText, leftMargin - 10, bottomMargin + 3);
             doc.text(footerText2, rightMargin + 10, bottomMargin + 3);
